3053c13c2e5e501d5881f7c097a937458eed6d1f
freddykembo <freddykembo@gmail.com>
2023-02-02 14:57:20 +0300

    Added CompanyFactory, update ContactFactory & DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +22,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call(CompaniesTableSeeder::class);
+
+        // Company::factory()->count(10)->create();
+        // Contact::factory()->count(50)->create();
+
+        Company::factory()->hasContacts(3)->count(10)->create();
     }
 }
